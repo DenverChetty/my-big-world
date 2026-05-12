0aca6e6d07bf782419cb7f8195ae8342bbc73895
DenverChetty <denver@dcis.co.za>
2026-05-12 19:37:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    target: 'es2022'
+    outDir: 'dist'
   }
 })

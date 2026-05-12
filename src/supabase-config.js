b4a01771cd1d0cb9d7526158915094e18aa04df0
DenverChetty <denver@dcis.co.za>
2026-05-12 18:12:04 +0200

    Update supabase-config.js

--- src/supabase-config.js
+++ src/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL=https://bmpiymiloblzowjqpcls.supabase.co;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY=sb_publishable_2cfNSLHmgkhylZQUz6ty9Q_YYSJe6tP;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
